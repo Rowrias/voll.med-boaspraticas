334cdcbf6d19a4bf5775e54c8669d75d42de2f99
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-10-07 09:04:34 -0300

    JSON Web Token
    - Adicionando a lib auth0 jwt
    - Gerando tokens JWT
    - Ajustes na geração do token

--- src/main/java/med/voll/api/domain/usuario/Usuario.java
+++ src/main/java/med/voll/api/domain/usuario/Usuario.java
@@ -60,5 +60,27 @@ public class Usuario implements UserDetails {
 	public boolean isEnabled() {
 		return true;
 	}
+	
+	// Getters Setters
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getLogin() {
+		return login;
+	}
+	public void setLogin(String login) {
+		this.login = login;
+	}
+	public String getSenha() {
+		return senha;
+	}
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	
 
 }
