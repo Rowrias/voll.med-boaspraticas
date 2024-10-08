db21bc86b80ec975f6161ffeaf92b89744976c86
Rodrigo Najdek <rodrigo.najdek@gmail.com>
2024-10-08 09:48:38 -0300

    Controle de acesso
    - Interceptando requisições
    - Criando o filter de segurança
    - Recuperando o token
    - Validando o token recebido
    - Autenticando o usuário

--- src/main/java/med/voll/api/controller/AutenticacaoController.java
+++ src/main/java/med/voll/api/controller/AutenticacaoController.java
@@ -27,12 +27,17 @@ public class AutenticacaoController {
 
 	@PostMapping
 	public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados) {
-		var authenticationtoken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
-		var authentication = manager.authenticate(authenticationtoken);
-		
-		var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
-		
-		return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+	    try {
+	        var authenticationToken = new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
+	        var authentication = manager.authenticate(authenticationToken);
+
+	        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+
+	        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	        return ResponseEntity.badRequest().body(e.getMessage());
+	    }
 	}
 	
 }
